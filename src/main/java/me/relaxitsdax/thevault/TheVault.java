3b8041caf6433bea671db5398644f13bee8e7d75
RelaxItsDax <relaxitsdax@gmail.com>
2024-05-18 13:30:52 -0400

    hello!

--- src/main/java/me/relaxitsdax/thevault/TheVault.java
+++ src/main/java/me/relaxitsdax/thevault/TheVault.java
@@ -1,12 +1,16 @@
 package me.relaxitsdax.thevault;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
 
 public final class TheVault extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        Bukkit.getLogger().log(Level.ALL, "Hi!");
 
     }
 
